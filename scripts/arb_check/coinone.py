c0a8c13fe0decfbeb20b36ac22e647b2c9c1d111
TobyG4 <toby_green4@hotmail.co.uk>
2018-04-29 12:49:41 +0200

    Remove secret_key

--- scripts/arb_check/coinone.py
+++ scripts/arb_check/coinone.py
@@ -5,11 +5,11 @@ import hmac
 import httplib2
 import time
 
-toby_ACCESS_TOKEN = '0d771f68-93d8-4b41-9434-a23b979ec9b8'
-toby_SECRET_KEY = 'c604fda7-12a5-43e7-8e35-e366c9434270'
+toby_ACCESS_TOKEN = '*'
+toby_SECRET_KEY = '*'
 
-ACCESS_TOKEN = '6e371d38-44ad-4065-9165-b147f1707704'
-SECRET_KEY = '36d4af29-7847-407e-9752-c6be06b05a54'
+ACCESS_TOKEN = '*'
+SECRET_KEY = '*'
 
 
 URL = 'https://api.coinone.co.kr/v1/transaction/coin/'
