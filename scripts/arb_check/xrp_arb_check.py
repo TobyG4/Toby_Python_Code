5caa60b0c1d70fa573bee409301e9f9e7f44c7d9
TobyG4 <toby_green4@hotmail.co.uk>
2018-04-29 12:51:21 +0200

    Update xrp_arb_check.py

--- scripts/arb_check/xrp_arb_check.py
+++ scripts/arb_check/xrp_arb_check.py
@@ -21,7 +21,7 @@ class ArbCheck(object):
         self.host = "159.89.20.249"
         self.database_name = 'crypto_test'
         self.user = 'toby'
-        self.password = 'R1i9p1p1l9e0$'
+        self.password = '*'
         self.database = DatabaseConnect(self.host, self.database_name, self.user, self.password, self.port)
         self.database.database_connect()
 
