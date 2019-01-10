6983b130ea1ef5aa978105f4a31afe95ef29a45b
TobyG4 <toby_green4@hotmail.co.uk>
2019-01-10 13:52:29 +0100

    Update setup.py

--- badjoke/setup.py
+++ badjoke/setup.py
@@ -1,12 +1,12 @@
 
 from setuptools import setup
 
-setup(name='funniest',
+setup(name='badjoke',
       version='0.1',
-      description='The funniest joke in the world',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
+      description='Worst joke in the world',
+      url='http://github.com/TobyG4/Toby_Python_Code/badjoke',
+      author='Tubbsy G',
+      author_email='',
       license='MIT',
-      packages=['funniest'],
+      packages=['badjoke'],
 zip_safe=False)
